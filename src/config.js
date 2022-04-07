4a7ead30a184a6ee4929d9429dd5e2cb9185f6ab
Chethan Yadav <rockchethan1111@gmail.com>
2022-04-08 01:39:22 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    token: "OTU5NTA5Njc5Mjc1NDA5NDI4.Ykc7Bg.CWTQ4xRJE7fEbp7qqNfhbUw3pow",  // your bot token
+    token: "TOKEN_HERE",  // your bot token
     prefix:  "!", // bot prefix
     ownerID: "491577179495333903", //your discord id
     mongourl: "mongodb+srv://RoshanGamer:public@cluster0.kyoy7.mongodb.net/Public1?retryWrites=true&w=majority", // MongoDb URL
