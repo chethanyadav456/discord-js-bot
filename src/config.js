b9ba5cb0b7298f67dcc85ed254c150ba817db74c
Chethan Yadav <rockchethan1111@gmail.com>
2022-04-08 21:48:55 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 module.exports = {
     token: "TOKEN_HERE",  // your bot token
     prefix:  "!", // bot prefix
-    ownerID: "491577179495333903", //your discord id
+    ownerID: "801478547893387345", //your discord id
     mongourl: "mongodb+srv://RoshanGamer:public@cluster0.kyoy7.mongodb.net/Public1?retryWrites=true&w=majority", // MongoDb URL
     embedColor: "#303236", // embed colour
-    logs: "875254787191504947", // channel id for guild create and delete logs
+    logs: "930820901069545582", // channel id for guild create and delete logs
 }
